ab892a7a50b2d4d00ededeff2309d1b7e923a498
Yurii_Popovskiy <ronondex663@gmail.com>
2025-05-18 11:13:52 +0300

    AddTestToEmail

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,46 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = validateEmail('test@mail.com');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
     expect(validateEmail('test838@gmail.com.'))
       .toBeTruthy();
+    expect(validateEmail('test@mail.com')).toBe(true);
   });
 
   // write more tests here
+  it(`should return 'false' if email doesn't contain '@' symbol`, () => {
+    expect(validateEmail('testmail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if email starts with a dot`, () => {
+    expect(validateEmail('.test@mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if personal_info has double dots`, () => {
+    expect(validateEmail('test..email@mail.com')).toBe(false);
+  });
+
+  it(`should return 'false' if domain starts with a dot`, () => {
+    expect(validateEmail('test@.mail.com')).toBe(false);
+  });
+
+  // eslint-disable-next-line max-len
+  it(`should return 'false' for email with forbidden special characters`, () => {
+    expect(validateEmail('te!st@mail.com')).toBe(false);
+    expect(validateEmail('te$st@mail.com')).toBe(false);
+    expect(validateEmail('te&st@mail.com')).toBe(false);
+  });
+
+  it(`should return 'true' for shortest valid email`, () => {
+    expect(validateEmail('t@q.c')).toBe(true);
+  });
+
+  it(`should return 'false' for email without domain`, () => {
+    expect(validateEmail('false@email')).toBe(false);
+  });
 });
